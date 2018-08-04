1f362ad2c09e0a84e978762b2ddc209d0272044d
aman singh <amansingh1932@gmail.com>
2018-08-05 02:41:21 +0530

    convert to smallest integer
    A number is given by user write a program to obtain smallest number after deleting i digits.

---
+++ convert_to_SmallestInteger.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<string.h>
+void deleteDigit(int*,int*,int,int,int);
+int main()
+{
+	char str[20];
+	int k;
+	int i=0;
+	printf("enter the number\n");
+	scanf("%s",str);
+	printf("enter the number of digits to delete\n");
+	scanf("%d",&k);
+	int len=strlen(str);
+	int input[len];
+	for(i=0;i<len;i++)
+	{
+		input[i]=str[i]-48;
+	}
+	int result[len];
+	deleteDigit(input,result,0,k+1,len);
+	for(i=0;i<len-k;i++)
+	{
+		printf("%d",result[i]);
+	}
+}
+void deleteDigit(int *input,int *result,int big,int end,int len)
+{
+	int min=big;
+	int i;
+	static int c=0;
+	for(i=big;i<end;i++)
+	{
+		if(c==0&&input[i]==0);
+		
+		else if(input[min]>input[i])
+			min=i;
+	}
+	result[c++]=input[min];
+	if(end<len)
+		deleteDigit(input,result,min+1,end+1,len);
+}
