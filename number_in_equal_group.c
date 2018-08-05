dc6b23266e9303840a3cecc364f3a3e27849284b
aman singh <amansingh1932@gmail.com>
2018-08-05 16:20:36 +0530

    program

---
+++ number_in_equal_group.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int min(int,int);
+int main()
+{
+	int n,a,b,i;
+	scanf("%d %d %d",&n,&a,&b);
+	if(n%2!=0)
+	{
+		printf("no");
+		return;
+	}
+	n=n/2;
+	int count[n+1];
+	int index[n+1];
+	int values[2];
+	values[0]=a;
+	values[1]=b;
+	for(i=0;i<=n;i++)
+	{
+		if(i==0)
+			count[i]=0;
+		else
+		{
+			count[i]=999999;
+			index[i]=-1;
+		}
+	}
+	int cal,j;
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<=n;j++)
+		{
+			if(j>=values[i])
+			{
+				cal=count[j-values[i]]+1;
+				count[j]=count[j]<cal?count[j]:cal;
+			}
+		}
+	}
+	if(count[j-1]==999999)
+		printf("NO");
+	else
+		printf("YES");
+}
+
